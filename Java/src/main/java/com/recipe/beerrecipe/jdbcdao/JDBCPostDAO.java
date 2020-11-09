2013e894da3beb8b7c459a8257ca062ae5ee0b9e
spennington45 <spennington45@gmail.com>
2020-11-09 17:03:17 -0600

    JDBCPostDAO filled out but not tested yet no front end

--- Java/src/main/java/com/recipe/beerrecipe/jdbcdao/JDBCPostDAO.java
+++ Java/src/main/java/com/recipe/beerrecipe/jdbcdao/JDBCPostDAO.java
@@ -1,51 +1,72 @@
 package com.recipe.beerrecipe.jdbcdao;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.recipe.beerrecipe.model.Post;
 
 public class JDBCPostDAO implements PostDAO {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
 	@Override
 	public List<Post> getAllPost() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Post> output = new ArrayList<Post>();
+		SqlRowSet results = jdbcTemplate.queryForRowSet("SELECT * FROM post");
+		while (results.next()) {
+			output.add(mapRowToPost(results));
+		}
+		return output;
 	}
 
 	@Override
 	public Post getPostById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		SqlRowSet results = jdbcTemplate.queryForRowSet("SELECT * FROM post WHERE id = ?", id);
+		return mapRowToPost(results);
 	}
 
 	@Override
 	public void updatePost(Post post) {
-		// TODO Auto-generated method stub
-
+		jdbcTemplate.update("UPDATE post title = ?, message = ?, imgurl = ?, postdate = ?, WHERE id = ?", 
+				post.getTitle(), post.getMessage(), post.getImageUrl(), post.getDate(), post.getId());
 	}
 
 	@Override
 	public void addPost(Post post) {
-		// TODO Auto-generated method stub
-
+		jdbcTemplate.update("INSERT INTO post (title, message, imgurl, postdate, user_id) VALUES (?, ?, ?, ?, ?)",
+				post.getTitle(), post.getMessage(), post.getImageUrl(), post.getDate(), post.getUserId());
 	}
 
 	@Override
 	public void deletePost(long id) {
-		// TODO Auto-generated method stub
-
+		jdbcTemplate.update("DELETE FROM post WHERE id = ?", id);
 	}
 
 	@Override
 	public void sticky(long id) {
-		// TODO Auto-generated method stub
-
+		jdbcTemplate.update("UPDATE post SET stickied = true WHERE id = ?", id);
 	}
 
 	@Override
 	public void unsticky(long id) {
-		// TODO Auto-generated method stub
-
+		jdbcTemplate.update("UPDATE post SET stickied = false WHERE id = ?", id);
 	}
 
+	public Post mapRowToPost(SqlRowSet results) {
+		Post post = new Post();
+		post.setId(results.getLong("id"));
+		post.setTitle(results.getString("title"));
+		post.setMessage(results.getString("message"));
+		post.setImageUrl(results.getString("imgUrl"));
+		post.setDate(results.getDate("postDate"));
+		post.setUserId(results.getInt("user_id"));
+		post.setStickied(results.getBoolean("stickied"));
+		return post;
+	}
+	
 }
